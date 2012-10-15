61ffd753cdc061c216665024fb2eac64a8172098
Michael Warkentin <mwarkentin@gmail.com>
2012-10-14 23:21:18 -0400

    Sample pixels from across the sample image

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var $ = function(el) {
 	return document.getElementById(el);
 };
 
+var x = 0,
+    y = 50,
+    width = 480,
+    height = 320,
+    sampleSize = 1;
+
 var sourceImage = $('SourceImage');
 console.log(sourceImage);
 var context = sourceImage.getContext('2d');
@@ -9,4 +15,8 @@ var source = new Image();
 source.src = 'sample-image.jpeg';
 source.onload = function() {
 	context.drawImage(source, 0, 0, sourceImage.width, sourceImage.height);
+	for (x=0; x < sourceImage.width; x++) {
+		console.log(x, y);
+		console.log(context.getImageData(x, y, sampleSize, sampleSize));
+	}
 };
