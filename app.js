7f7f28910b354eae1b2463ee742a447312f780aa
Michael Warkentin <mwarkentin@gmail.com>
2012-10-15 00:19:01 -0400

    Add sampling by clicking on the source image

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-var $ = function(el) {
-	return document.getElementById(el);
-};
-
 var x = 0,
     y = 75,
     width = 480,
     height = 320,
     sampleSize = 1;
 
-var sourceCanvas = $('Source'),
-    destinationCanvas = $('Destination');
+var sourceCanvas = $('#Source')[0],
+    destinationCanvas = $('#Destination')[0];
+
+console.log(sourceCanvas);
 
 var sourceContext = sourceCanvas.getContext('2d'),
     destinationContext = destinationCanvas.getContext('2d');
@@ -18,6 +16,16 @@ var sample = new Image();
 sample.src = 'sample-image.jpeg';
 sample.onload = function() {
 	sourceContext.drawImage(sample, 0, 0, sourceCanvas.width, sourceCanvas.height);
+	drawToDestination(y);
+};
+
+$(sourceCanvas).on('click', function(e) {
+	var yClick = e.y - e.target.offsetTop;
+	drawToDestination(yClick);
+});
+
+var drawToDestination = function(y) {
+	console.log('Sampling at ' + y + 'px');
 	for (x=0; x < sourceCanvas.width; x++) {
 		pixelData = sourceContext.getImageData(x, y, sampleSize, sampleSize);
 		for (var yy = 0; yy < destinationCanvas.height; yy++) {
