52144dafd6fc55dbce96e140d5d7c11fa3d6cb57
Michael Warkentin <mwarkentin@gmail.com>
2012-10-15 08:34:04 -0400

    Fix sample height and draw line where sample was taken
    * Remove canvas border and header
    * Set canvas width / height in html
    * Get rid of for loop to sample across image - just use a single
      sample that covers the whole image, 1px high
    * Add function that draws line where the sample came from

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var x = 0,
 var sourceCanvas = $('#Source')[0],
     destinationCanvas = $('#Destination')[0];
 
-console.log(sourceCanvas);
-
 var sourceContext = sourceCanvas.getContext('2d'),
     destinationContext = destinationCanvas.getContext('2d');
 
@@ -17,19 +15,26 @@ sample.src = 'sample-image.jpeg';
 sample.onload = function() {
 	sourceContext.drawImage(sample, 0, 0, sourceCanvas.width, sourceCanvas.height);
 	drawToDestination(y);
+	drawSampleLine(y);
 };
 
 $(sourceCanvas).on('click', function(e) {
 	var yClick = e.y - e.target.offsetTop;
 	drawToDestination(yClick);
+	drawSampleLine(yClick);
 });
 
 var drawToDestination = function(y) {
-	console.log('Sampling at ' + y + 'px');
-	for (x=0; x < sourceCanvas.width; x++) {
-		pixelData = sourceContext.getImageData(x, y, sampleSize, sampleSize);
-		for (var yy = 0; yy < destinationCanvas.height; yy++) {
-			destinationContext.putImageData(pixelData, x, yy);
-		}
+	var pixelData = sourceContext.getImageData(x, y, width, sampleSize);
+	for (var yy = 0; yy < destinationCanvas.height; yy++) {
+		destinationContext.putImageData(pixelData, x, yy);
 	}
+};
+
+var drawSampleLine = function(y) {
+	sourceContext.beginPath();
+	sourceContext.moveTo(0, y);
+	sourceContext.lineTo(width, y);
+	sourceContext.closePath();
+	sourceContext.stroke();
 };
