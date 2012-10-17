a965098d95eadd3d5e8991c399c9cec84d29d83a
Michael Warkentin <mwarkentin@gmail.com>
2012-10-17 08:45:17 -0400

    Fix #6: Use new image by dragging it onto drop target
    * Add filepicker.io js lib
    * Add drop target to top of page and style it
    * Add all necessary event handlers (some are just console.log() for
      now)
    * On drop, upload image to filepicker, read it back, and update
      sourceImage.src

--- app.js
+++ app.js
@@ -10,41 +10,75 @@ var sourceCanvas = $('#Source')[0],
 var sourceContext = sourceCanvas.getContext('2d'),
     destinationContext = destinationCanvas.getContext('2d');
 
-var sample = new Image();
-sample.src = 'sample-image.jpeg';
-sample.onload = function() {
-	sourceContext.drawImage(sample, 0, 0, sourceCanvas.width, sourceCanvas.height);
-	drawToDestination(y);
+var sourceImage = new Image();
+sourceImage.src = 'sample-image.jpeg';
+sourceImage.onload = function() {
+    sourceContext.drawImage(sourceImage, 0, 0, sourceCanvas.width, sourceCanvas.height);
+    drawToDestination(y);
 };
 
 $(sourceCanvas).on({
-	mousemove: function(e) {
-		var y = e.y - e.target.offsetTop;
-		drawToDestination(y);
-	},
-	click: function(e) {
-		var img = generatePng(destinationCanvas);
-		$('body').append(img);
-	}
+    mousemove: function(e) {
+        var y = e.y - e.target.offsetTop;
+        drawToDestination(y);
+    },
+    click: function(e) {
+        var img = generatePng(destinationCanvas);
+        $('body').append(img);
+    }
 });
 
 var drawToDestination = function(y) {
-	var pixelData = sourceContext.getImageData(x, y, width, sampleSize);
-	for (var yy = 0; yy < destinationCanvas.height; yy++) {
-		destinationContext.putImageData(pixelData, x, yy);
-	}
+    var pixelData = sourceContext.getImageData(x, y, width, sampleSize);
+    for (var yy = 0; yy < destinationCanvas.height; yy++) {
+        destinationContext.putImageData(pixelData, x, yy);
+    }
 };
 
 var drawSampleLine = function(y) {
-	sourceContext.beginPath();
-	sourceContext.moveTo(0, y);
-	sourceContext.lineTo(width, y);
-	sourceContext.closePath();
-	sourceContext.stroke();
+    sourceContext.beginPath();
+    sourceContext.moveTo(0, y);
+    sourceContext.lineTo(width, y);
+    sourceContext.closePath();
+    sourceContext.stroke();
 };
 
 var generatePng = function(canvas) {
-	var img = new Image();
-	img.src = canvas.toDataURL();
-	return img;
+    var img = new Image();
+    img.src = canvas.toDataURL();
+    return img;
+};
+
+var handleDragEnter = function() {
+    console.log('hello world!');
+};
+
+var handleDragLeave = function() {
+    console.log('goodbye world!');
+};
+
+var handleOnStart = function(files) {
+    console.log('starting upload: ', files);
+};
+
+var handleOnSuccess = function(files) {
+    console.log('upload success: ', files);
+    var file = files[0];
+    getUploadedImg(file.url, file.type);
+};
+
+var handleOnProgress = function(percentage) {
+    console.log('upload progress: ' + percentage + '%');
+};
+
+var handleOnError = function(type, message) {
+    console.log('error: ', type, message);
+};
+
+var getUploadedImg = function(url, type) {
+    filepicker.read(url, {base64encode: true}, function(data) {
+        var mime = 'data:' + type + ';base64,';
+        var imgData = mime + data;
+        sourceImage.src = imgData;
+    });
 };
